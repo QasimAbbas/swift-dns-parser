a1d69c8a9665ff8db8277764392e93c61f8b3935
Qasim Abbas <qasim.abbas@rutgers.edu>
2018-10-28 13:11:02 -0400

    Initial Commit

---
+++ swift-dns-parser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-dns-parser
+//
+//  Created by Qasim Abbas on 10/28/18.
+//  Copyright © 2018 Qasim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
